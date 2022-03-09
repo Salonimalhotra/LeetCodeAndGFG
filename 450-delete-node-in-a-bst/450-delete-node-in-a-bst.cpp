bbab50b84fccb81ab295ad462ef928817ebd242d
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-03-09 20:03:28 +0530

    Time: 49 ms (51.68%), Space: 32.7 MB (76.72%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool search(TreeNode*root,int key){
+        if(root==NULL){
+         return false;
+        }
+        if(root->val==key){
+            return true;
+        }
+        return search(root->left,key) || search(root->right,key);
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+       
+        if(search(root,key)==false || root==NULL){
+            return root;
+        }
+    
+        else if(root->val<key){
+            root->right=deleteNode(root->right,key);
+            return root;
+        }
+        else if(root->val>key){
+            root->left=deleteNode(root->left,key);
+            return root;
+        }
+        else if(root->val==key){
+            if(root->left==NULL && root->right==NULL){
+                delete root;
+                return NULL;
+            }
+            else if(root->left==NULL && root->right!=NULL){
+              TreeNode * temp=root->right;
+                root->right=NULL;
+                delete root;
+                return temp;
+            }
+            else if(root->left!=NULL && root->right==NULL){
+              TreeNode * temp=root->left;
+                root->left=NULL;
+                delete root;
+                return temp;
+            }
+            else{
+                TreeNode * temp=root->right;
+                while(temp->left!=NULL){
+                 temp=temp->left;  
+                }
+                int min_data=temp->val;
+                root->val=min_data;
+                root->right=deleteNode(root->right,min_data);
+                return root;
+            }
+        }
+        return root;
+    }
+};
