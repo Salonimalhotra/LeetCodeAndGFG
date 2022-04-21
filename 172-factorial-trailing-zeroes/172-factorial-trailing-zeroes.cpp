fe58e6e8c96af291d4e8d9116042720368b62664
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-04-21 22:40:28 +0530

    Time: 3 ms (47.72%), Space: 5.7 MB (95.17%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int res=0;
+        int count1=0;
         for(int i=5;i<=n;i*=5){
-            res+=n/i;
+            count1+=n/i;
         }
-        return res;
+        int count2=0;
+        for(int j=2;j<=n;j*=2){
+            count2+=n/j;
+        }
+        return min(count1,count2);
     }
 };
