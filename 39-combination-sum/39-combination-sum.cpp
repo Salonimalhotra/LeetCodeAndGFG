7fb78e7a7a7a3b96af2422dc74c096437d2be661
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-04-21 14:00:50 +0530

    Time: 4 ms (91.61%), Space: 10.8 MB (88.80%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void solve(int index, int target, vector<int>& candidates, vector<vector<int>>& ans, vector<int>& x){
-        if(index == candidates.size()){ // when we reach the end of the array
-            if(target == 0){ // if our very own target has a specified combination to match it we push it into our final answer
-                ans.push_back(x);
+    void helper(vector<int>&candidates,vector<int>&output,vector<vector<int>>&ans,int target,int index){
+        if(index==candidates.size()){
+            if(target==0){
+                ans.push_back(output);
             }
             return;
         }
-        
-    if(candidates[index] <= target){ // if the current element is less than or equal to target, we push it into our data struct
-        x.push_back(candidates[index]);
-        solve(index, target - candidates[index], candidates, ans, x); // we then give a recursive call with a different target and the same index until we break either condition
-        x.pop_back();
+        if(candidates[index]<=target){
+            output.push_back(candidates[index]);
+            helper(candidates,output,ans,target-candidates[index],index);
+            output.pop_back();
+        }
+        helper(candidates,output,ans,target,index+1);
+        return;
     }
-    solve(index+1, target, candidates, ans, x); // if the condition breaks we move to the next index
-    }
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
-        vector<int>x;
-        solve(0, target, candidates, ans, x);
+        vector<int>output;
+        helper(candidates,output,ans,target,0);
         return ans;
     }
 };
