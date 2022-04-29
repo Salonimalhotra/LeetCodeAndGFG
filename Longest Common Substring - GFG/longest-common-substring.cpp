af4dc2cf1a9545973c1e7a0af30722e531426df3
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-04-29 23:47:37 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -6,24 +6,21 @@ using namespace std;
 class Solution{
     public:
     
-    int longestCommonSubstr (string S1, string S2, int n, int m)
+    int longestCommonSubstr (string s1, string s2, int x, int y)
     {
-        // your code here
-        n=S1.size();
-        m=S2.size();
         
-        int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
+        int dp[x+1][y+1];
+        for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
         }
         
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
-        }
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(S1[i-1]==S2[j-1]){
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=y;j++){
+                if(s1[i-1]==s2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -32,13 +29,15 @@ class Solution{
             }
         }
         
-        int max_ans=0;
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                max_ans=max(max_ans,dp[i][j]);
+        int ans=INT_MIN;
+        for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                ans=max(ans,dp[i][j]);
             }
         }
-        return max_ans;
+        return ans;
+      
+    
     }
 };
 
