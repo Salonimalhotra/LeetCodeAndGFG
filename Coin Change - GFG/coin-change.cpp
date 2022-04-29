3c630c584c951cd920eef1eefc110e02edff1ff1
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-04-29 20:20:21 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,38 +5,59 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    long long int helper(int S[],int m,int n,long long int **dp){
-        if(n==0){
-            return 1;
-        }
-        if(m==0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        //if we are taking the current coin;
-        if(S[m-1]<=n){
-        long long int x=helper(S,m,n-S[m-1],dp);
-        long long int y=helper(S,m-1,n,dp);
-        return dp[m][n]=x+y;
-        }
-        else{
-            return  dp[m][n]=helper(S,m-1,n,dp);
-        }
-    }
-    long long int count(int S[], int m, int n) {
-
-        // code here.
-          long long int **dp=new long long int*[m+1];
-          for(int i=0;i<=m;i++){
-           dp[i]=new long long int[n+1];
-             for(int j=0;j<=n;j++){
-               dp[i][j]=-1;
-              }
+     long long int count(int S[], int m, int n) {
+         long long int dp[m+1][n+1];
+         
+         for(int i=0;i<=m;i++){
+           dp[i][0]=1;    
          }
-         return helper(S,m,n,dp);
-    }
+         for(int j=1;j<=n;j++){
+             dp[0][j]=0;
+         }
+         for(int i=1;i<=m;i++){
+             for(int j=1;j<=n;j++){
+                 if(S[i-1]<=j){
+                     dp[i][j]=dp[i-1][j]+dp[i][j-S[i-1]];
+                 }
+                 else{
+                     dp[i][j]=dp[i-1][j];
+                 }
+             }
+         }
+         return dp[m][n];
+     }
+    // long long int helper(int S[],int m,int n,long long int **dp){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(m==0){
+    //         return 0;
+    //     }
+    //     if(dp[m][n]!=-1){
+    //         return dp[m][n];
+    //     }
+    //     //if we are taking the current coin;
+    //     if(S[m-1]<=n){
+    //     long long int x=helper(S,m,n-S[m-1],dp);
+    //     long long int y=helper(S,m-1,n,dp);
+    //     return dp[m][n]=x+y;
+    //     }
+    //     else{
+    //         return  dp[m][n]=helper(S,m-1,n,dp);
+    //     }
+    // }
+    // long long int count(int S[], int m, int n) {
+
+    //     // code here.
+    //       long long int **dp=new long long int*[m+1];
+    //       for(int i=0;i<=m;i++){
+    //       dp[i]=new long long int[n+1];
+    //          for(int j=0;j<=n;j++){
+    //           dp[i][j]=-1;
+    //           }
+    //      }
+    //      return helper(S,m,n,dp);
+    // }
 };
 
 // { Driver Code Starts.
