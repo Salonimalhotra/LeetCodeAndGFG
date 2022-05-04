16e6baa26fc8f034d63ea77c813e754bf08f224c
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-05-05 00:08:37 +0530

    Added solution - LeetHub

--- Swap and Maximize - GFG/swap-and-maximize.cpp
+++ Swap and Maximize - GFG/swap-and-maximize.cpp
@@ -22,37 +22,19 @@ int main()
 
 long long int maxSum(int arr[], int n)
 {
-    sort(arr,arr+n);
-    int arr1[n];
-    for(int i=0;i<n;i++){
-        arr1[i]=arr[i];
-    }
-    
-    int arr2[n];
-    sort(arr,arr+n,greater<int>());
-     for(int i=0;i<n;i++){
-        arr2[i]=arr[i];
-    }
-    int finalArr[n];
-    int i=0;
-    int j=0;
-    int m=0;
-    while(m!=n){
-        if(m%2==0){
-            finalArr[m]=arr1[i];
-            i++;
-        }
-        else{
-            finalArr[m]=arr2[j];
-            j++;
-        }
-        m++;
-    }
-    long long int sum=0;
-    for(int i=0;i<=n-2;i++){
-        sum+=abs(finalArr[i]-finalArr[i+1]);
-    }
-    sum+=abs(finalArr[n-1]-finalArr[0]);
-    return sum;
+     sort(arr,arr+n);
+   int i=0;
+   int j= n-1;
+   long long int sum = 0;
+   while(i<j)
+   {
+       sum += abs(arr[i]-arr[j]);
+       sum += abs(arr[i+1]-arr[j]);
+       i++;
+       j--;
+   }
+   sum += abs(arr[0]-arr[i]);
+   
+   return sum;
     
 }
