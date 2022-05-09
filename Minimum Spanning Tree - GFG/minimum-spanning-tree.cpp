b1344170cf1bf4929a4e6a6df674d68ee03802c8
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-05-09 10:47:09 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -4,67 +4,101 @@ using namespace std;
 
  // } Driver Code Ends
 
-
+class Edge{
+  public:
+  int source;
+  int dest;
+  int weight;
+  Edge(int s,int d,int w){
+      source=s;
+      dest=d;
+      weight=w;
+  }
+  
+};
+bool compare(Edge e1,Edge e2){
+    if(e1.weight<e2.weight){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int findParent(int x,int *parent){
+    if(x==parent[x]){
+        return x;
+    }
+    return parent[x]=findParent(parent[x],parent);
+}
+void union1(int x,int y,int *parent,int *rank){
+    x=findParent(x,parent);
+    y=findParent(y,parent);
+    
+    if(rank[x]<rank[y]){
+        parent[x]=y;
+    }
+    else if(rank[x]>rank[y]){
+        parent[y]=x;
+    }
+    else{
+        parent[x]=y;
+        rank[y]++;
+    }
+    return;
+}
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-         vector<pair<int,int>>newadj[V];
-         for(int i=0;i<V;i++){
-                vector<pair<int,int>>t1;
-             for(int j=0;j<adj[i].size();j++){
-                 int adjVertex=adj[i][j][0];
-                 int adjWeight=adj[i][j][1];
-                 pair<int,int>p1;
-                 p1.first=adjVertex;
-                 p1.second=adjWeight;
-                 t1.push_back(p1);
-             }
-             newadj[i]=t1;
-         }
-         
-         int *weights=new int[V];
-         for(int i=0;i<V;i++){
-             weights[i]=INT_MAX;
-         }
-         weights[0]=0;
-         
-         int * parents=new int[V];
-         for(int i=0;i<V;i++){
-             parents[i]=-1;
-         }
-         
-         bool * visited=new bool[V];
-         for(int i=0;i<V;i++){
-             visited[i]=false;
-         }
-         
-         for(int i=0;i<V;i++){
-             int minWeight=INT_MAX;
-             int min_Vertex=-1;
-             for(int i=0;i<V;i++){
-                 if(visited[i]!=true && weights[i]<minWeight){
-                     minWeight=weights[i];
-                     min_Vertex=i;
-                 }
-             }
-             visited[min_Vertex]=true;
-             for(int i=0;i<newadj[min_Vertex].size();i++){
-                 if(visited[newadj[min_Vertex][i].first]!=true && weights[newadj[min_Vertex][i].first]>newadj[min_Vertex][i].second){
-                     weights[newadj[min_Vertex][i].first]=newadj[min_Vertex][i].second;
-                     parents[newadj[min_Vertex][i].first]=min_Vertex;
-                 }
-             }
-         }
-         int finalAns=0;
-         for(int i=0;i<V;i++){
-             finalAns+=weights[i];
-         }
-         return finalAns;
+        vector<Edge>edges;
+        for(int i=0;i<V;i++){
+            for(int j=0;j<adj[i].size();j++){
+                Edge edge1(i,adj[i][j][0],adj[i][j][1]);
+                // edge1.source=i;
+                // edge1.dest=adj[i][j][0];
+                // edge1.weight=adj[i][j][1];
+                edges.push_back(edge1);
+            }
+            
+        }
+        
+        sort(edges.begin(),edges.end(),compare);
+        
+        int * parent=new int [V];
+        int * rank=new int[V];
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+        
+        int x=0;
+        int cost=0;
+        vector<Edge>res;
+        int i=0;
+          while(i<V-1){
+              Edge e1=edges[x];
+              int source=e1.source;
+              int destination=e1.dest;
+              int weight=e1.weight;
+              
+              if(findParent(source,parent)==findParent(destination,parent)){
+                  x++;
+              }
+              else{
+                  res.push_back(e1);
+                  cost+=weight;
+                  union1(source,destination,parent,rank);
+                  x++;
+                  i++;
+              }
+        }
+        // for(int i=0;i<res.size();i++){
+        //     cout<<res[i].source<<" "<<res[i].dest<<" "<<res[i].weight<<endl;
+        // }
+        return cost;
     }
 };
 
