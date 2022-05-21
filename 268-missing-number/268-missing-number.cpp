b8edf5b89fdb4b86c8a9cb7bff66a1eaa875eb8e
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-05-21 18:40:23 +0530

    Time: 28 ms (45.94%), Space: 18.1 MB (21.08%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,18 +1,31 @@
 #include<unordered_map>
 #include<vector>
+#include<bits/stdc++.h>
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         unordered_map<int,bool>map;
-        for(int i=0;i<nums.size();i++){
-            map[nums[i]]=true;
+        int i=0;
+        while(i<nums.size()){
+            int correct=nums[i];
+            if(correct<nums.size() && nums[i]!=nums[correct]){
+                swap(nums[i],nums[correct]);
+            }
+            else{
+                i++;
+            }
         }
-        int ans=-1;
-        for(int i=0;i<=nums.size();i++){
-            if(map.find(i)==map.end()){
+       int ans=-1;
+        bool check=true;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i){
                 ans=i;
+                check=false;
                 break;
             }
+        }
+        if(check==true){
+            return nums.size();
         }
         return ans;
     }
