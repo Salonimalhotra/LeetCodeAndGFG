0c697d9691ce7b614308fc0a9a5da5312aefa6e5
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-05-29 18:26:40 +0530

    Time: 304 ms (15.24%), Space: 28.6 MB (53.98%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,35 +1,61 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
+    int nextgap(int gap){
+        if(gap<=1){
+            return 0;
+        }
+        return (int)ceil(gap / 2.0);
+    }
+    void swap(vector<int>&nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        return;
+    }
     void merge(vector<int>&nums,int s,int mid,int e){
-        int len=e-s+1;
-        vector<int>arr(len,0);
-        int i=s;
-        int j=mid+1;
-        int k=0;
-        while(i<=mid && j<=e){
-            if(nums[i]<=nums[j]){
-                arr[k]=nums[i];
-                k++;
-                i++;
-            }
-            else{
-                arr[k++]=nums[j++];
-            }
-        }
+//         int len=e-s+1;
+//         vector<int>arr(len,0);
+//         int i=s;
+//         int j=mid+1;
+//         int k=0;
+//         while(i<=mid && j<=e){
+//             if(nums[i]<=nums[j]){
+//                 arr[k]=nums[i];
+//                 k++;
+//                 i++;
+//             }
+//             else{
+//                 arr[k++]=nums[j++];
+//             }
+//         }
         
-        while(i<=mid){
-             arr[k]=nums[i];
-                k++;
-                i++;
-        }
+//         while(i<=mid){
+//              arr[k]=nums[i];
+//                 k++;
+//                 i++;
+//         }
         
-        while(j<=e){
-             arr[k++]=nums[j++];
-        }
-        k=0;
-        for(int i=s;i<=e;i++){
-            nums[i]=arr[k++];
+//         while(j<=e){
+//              arr[k++]=nums[j++];
+//         }
+//         k=0;
+//         for(int i=s;i<=e;i++){
+//             nums[i]=arr[k++];
+//         }
+//         return;
+        
+        int gap=e-s+1;
+        for(gap=nextgap(gap);gap>0;gap=nextgap(gap)){
+           int i=s;
+           int j=s+gap;
+           while(i<=e && j<=e){
+               if(nums[i]>nums[j]){
+                 swap(nums,i,j);
+               }
+               i++;
+               j++;
+           }
         }
         return;
     }
