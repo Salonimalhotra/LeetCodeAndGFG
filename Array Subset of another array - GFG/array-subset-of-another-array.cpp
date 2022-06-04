2dc3f0aca9082f4dc9672ccc8efda33b142dde83
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-04 14:51:22 +0530

    Added solution - LeetHub

---
+++ Array Subset of another array - GFG/array-subset-of-another-array.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) ;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a1[n], a2[m];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a1[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> a2[i];
+        }
+
+        cout << isSubset(a1, a2, n, m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+#include<unordered_map>
+string isSubset(int a1[], int a2[], int n, int m) {
+    bool check=true;
+    for(int i=0;i<m;i++){
+        int x=a2[i];
+        bool flag=false;
+        for(int j=0;j<n;j++){
+            if(a1[j]==x){
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false){
+            return "No";
+        }
+    }
+    return "Yes";
+}
