499ea77ea41da61edacec4de69039538b017239f
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-06 17:48:11 +0530

    Time: 7 ms (45.59%), Space: 9.4 MB (78.81%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-       int i=0;
-       int j=matrix[0].size()-1;
-        while(i>=0 && j>=0 && i<matrix.size() && j<matrix[0].size()){
-            if(matrix[i][j]==target){
-                return true;
+       // int i=0;
+       // int j=matrix[0].size()-1;
+       //  while(i>=0 && j>=0 && i<matrix.size() && j<matrix[0].size()){
+       //      if(matrix[i][j]==target){
+       //          return true;
+       //      }
+       //      else if(matrix[i][j]>target){
+       //          j--;
+       //      }
+       //      else if(matrix[i][j]<target){
+       //          i++;
+       //      }
+       //  }
+       //  return false;
+        
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        int i=0;
+        int j=rows*cols -1;
+        int mid=i+(j-i)/2;
+        while(i<=j){
+            //as we know when stored in a linear order the element is store like :- i*c+j;
+            //therefore to get to know the col index would be 
+            int element=matrix[mid/cols][mid%cols];
+            if(element==target){
+                return 1;
             }
-            else if(matrix[i][j]>target){
-                j--;
+            else if(element<target){
+                i=mid+1;
             }
-            else if(matrix[i][j]<target){
-                i++;
+            else if(element>target){
+                j=mid-1;
             }
+            mid=i+(j-i)/2;
         }
-        return false;
+        return 0;
     }
 };
