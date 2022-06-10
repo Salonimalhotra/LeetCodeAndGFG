edd8bc3ddc8eb2ecce78fbe617f4b0ec266eac2d
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-10 06:30:45 +0530

    Added solution - LeetHub

--- Find Pair Given Difference - GFG/find-pair-given-difference.cpp
+++ Find Pair Given Difference - GFG/find-pair-given-difference.cpp
@@ -26,32 +26,33 @@ int main()
     return 0;
 }// } Driver Code Ends
 
-#include<unordered_map>
+int binarySearch(int arr[],int s,int e,int x){
+    int i=s;
+    int j=e;
+    while(i<=j){
+        int mid=i+(j-i)/2;
+        if(arr[mid]==x){
+            return mid;
+        }
+        else if(arr[mid]<x){
+            i=mid+1;
+        }
+        else if(arr[mid]>x){
+            j=mid-1;
+        }
+    }
+    return -1;
+}
 bool findPair(int arr[], int size, int n){
     //code
-    if(n==0){
-        unordered_map<int,int>map1;
-        for(int i=0;i<size;i++){
-            map1[arr[i]]++;
-        }
-        for(auto i:map1){
-            if(i.second>=2){
-                return true;
-            }
-        }
-        return false;
-    }
-    unordered_map<int,int>map;
+    sort(arr,arr+size);
     for(int i=0;i<size;i++){
-        map[arr[i]]=i;
-    }
-    bool check=false;
-    for(int i=0;i<size;i++){
-        if((map.find(arr[i]+n)!=map.end()) || (map.find(arr[i]-n)!=map.end())){
-            check=true;
-            break;
+        //arr[i]-x=n => x=arr[i]-n 
+        //x-arr[i]=n => x=n+arr[i]
+        if(binarySearch(arr,i+1,size-1,arr[i]-n)!=-1 || binarySearch(arr,i+1,size-1,n+arr[i])!=-1){
+            return true;
         }
     }
-    return check;
+    return false;
     
 }
