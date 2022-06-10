4d6f25f16a0d40d8f3b742fe906fa1c41fec6941
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-10 06:38:37 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -11,15 +11,13 @@ class Solution
     int FindMaxSum(int arr[], int n)
     {
         // Your code here
-        int *dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=arr[0];
-        for(int i=2;i<=n;i++)
-        {
-           dp[i]=max(dp[i-1],dp[i-2]+arr[i-1]);
+        int dp[n]={0};
+        dp[0]=arr[0];
+        dp[1]=max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+arr[i]);
         }
-        
-        return dp[n];
+        return dp[n-1];
     }
 };
 
