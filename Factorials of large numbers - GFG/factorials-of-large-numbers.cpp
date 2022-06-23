5d6c5cab104dad5487f82c71d6e8e2d81fad9d02
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-23 23:14:37 +0530

    Added solution - LeetHub

--- Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
+++ Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
@@ -6,42 +6,41 @@ using namespace std;
  // } Driver Code Ends
 //User function template for C++
 class Node{
-    public:
-    int data;
-    Node * prev;
-    Node(int x){
-        data=x;
-        prev=NULL;
-    }
+  public:
+  int data;
+  Node * prev;
+  Node(int d){
+      this->data=d;
+      prev=NULL;
+  }
 };
-void multiply(Node * tail,int n){
+void multiply(Node * n1,int x){
     int carry=0;
-    Node * temp=tail;
-    Node * previous=tail;
+    Node * temp=n1;
+    Node * prevtemp=n1;
     while(temp!=NULL){
-        int data=temp->data*n+carry;
+        int data=temp->data*x+carry;
         temp->data=data%10;
         carry=data/10;
-        previous=temp;
+        prevtemp=temp;
         temp=temp->prev;
     }
-    
     if(carry!=0){
-        while(carry!=0){
+        while(carry!=0 ){
             Node * newNode=new Node(carry%10);
+            prevtemp->prev=newNode;
             carry=carry/10;
-            previous->prev=newNode;
-            previous=previous->prev;
+            prevtemp=prevtemp->prev;
         }
     }
     return;
 }
-void helper(vector<int>&ans,Node * tail){
-    if(tail==NULL){
-        return;
+void fill(vector<int>&ans,Node * n1){
+    if(n1==NULL){
+        return ;
     }
-    helper(ans,tail->prev);
-    ans.push_back(tail->data);
+    fill(ans,n1->prev);
+    ans.push_back(n1->data);
     return;
 }
 class Solution {
@@ -49,12 +48,17 @@ public:
     vector<int> factorial(int N){
         // code here
         vector<int>ans;
-        Node * tail=new Node(1);
-        for(int i=2;i<=N;i++){
-            multiply(tail,i);
+        if(N==0 || N==1){
+            ans.push_back(1);
+            return ans;
         }
-        helper(ans,tail);
+        Node *n1=new Node(1);
+        for(int i=2;i<=N;i++){
+            multiply(n1,i);
+        }
+        fill(ans,n1);
         return ans;
+        
     }
 };
 
