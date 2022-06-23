e503f9b48d0c1a1d3316a6ebb623b2def2cf1f97
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-23 17:30:36 +0530

    Time: 7 ms (55.56%), Space: 12.2 MB (36.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,46 +1,39 @@
 #include<vector>
-#include<algorithm>
 class Solution {
 public:
     void reverse(vector<int>&nums,int s,int e){
-        while(s<e){
-            swap(nums[s],nums[e]);
-               s++;
-               e--;          
+        int i=s;
+        int j=e;
+        while(i<=j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
         }
         return;
     }
     void nextPermutation(vector<int>& nums) {
-       // using stl
-        // next_permutation(nums.begin(),nums.end());
-        // return;
-        bool flag=false;
-        int indx1;
+        int idx=-1;
+        bool flag=true;
         for(int i=nums.size()-1;i>0;i--){
-            if(nums[i-1]<nums[i]){
-                flag=true;
-                indx1=i-1;
+            if(nums[i]>nums[i-1]){
+                idx=i-1;
+                flag=false;
                 break;
             }
         }
-        
-        if(flag==false){
-            reverse(nums,0,nums.size()-1);
+        if(flag){
+            return reverse(nums,0,nums.size()-1);
         }
-        
-        else{
-            // int indx1=i-1;
-            int indx2;
-            for(int j=nums.size()-1;j>indx1;j--){
-                if(nums[j]>nums[indx1]){
-                    indx2=j;
-                    break;
-                }
+        int x;
+        for(int i=nums.size()-1;i>idx;i--){
+            if(nums[i]>nums[idx]){
+                x=i;
+                break;
             }
-            swap(nums[indx1],nums[indx2]);
-            reverse(nums,indx1+1,nums.size()-1);
         }
+        swap(nums[idx],nums[x]);
+        reverse(nums,idx+1,nums.size()-1);
         return;
+        
     }
-    
 };
