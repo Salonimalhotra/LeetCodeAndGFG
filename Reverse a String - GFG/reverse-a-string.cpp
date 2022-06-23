b2f1be77e63850d03fdbcfde83b98fdde7f730d0
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-06-23 14:01:59 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -33,9 +33,22 @@ string reverseWord(string str){
     
   //Your code here
   //Brute force Approach:-
-  string ans;
-  for(int i=str.size()-1;i>=0;i--){
-      ans.push_back(str[i]);
+  // time - O(n) && space is O(n)
+//   string ans;
+//   for(int i=str.size()-1;i>=0;i--){
+//       ans.push_back(str[i]);
+//   }
+
+//   return ans;
+// optimised - time -O(n) && space is O(1)
+  int i=0;
+  int j=str.size()-1;
+  while(i<=j){
+      char temp=str[i];
+      str[i]=str[j];
+      str[j]=temp;
+      i++;
+      j--;
   }
-  return ans;
+  return str;
 }
