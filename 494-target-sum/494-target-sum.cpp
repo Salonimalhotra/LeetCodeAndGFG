cef28db5c5091aa1619982da44cb783f61bc6809
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-03 00:09:24 +0530

    Time: 7 ms (96.43%), Space: 12.1 MB (41.30%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    int CountSubsets(vector<int>&nums,int sum){
-        int dp[nums.size()+1][sum+1];
-        for(int i=0;i<nums.size();i++){
+    int countSubsetSum(vector<int>& nums, int subsetSum){
+        int n =nums.size();
+        int ** dp=new int*[nums.size()+1];
+        for(int i=0;i<=nums.size();i++){
+            dp[i]=new int[subsetSum+1];
+        }
+        
+       for(int i=0;i<=nums.size();i++){
             dp[i][0]=1;
         }
-        for(int j=1;j<=sum;j++){
+        for(int j=1;j<=subsetSum;j++){
             dp[0][j]=0;
         }
-        for(int i=1;i<=nums.size();i++){
-            for(int j=0;j<=sum;j++){
+        
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=subsetSum;j++){
                 if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-(nums[i-1])];
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
                 }
-                else if(nums[i-1]>j){
+                else{
                     dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        
-        return dp[nums.size()][sum];
+        return dp[n][subsetSum];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum=0;
+         int totalSum=0;
         for(int i=0;i<nums.size();i++){
             totalSum+=nums[i];
         }
@@ -33,7 +38,7 @@ public:
         if(subset_Sum<0 ||(totalSum+target)%2!=0){
            return 0;
         }
-        int ans=CountSubsets(nums,subset_Sum);
+        int ans=countSubsetSum(nums,subset_Sum);
         return ans;
     }
 };
