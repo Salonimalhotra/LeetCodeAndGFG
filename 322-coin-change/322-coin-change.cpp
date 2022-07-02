ea698cdc990d4af7d314de170681deb6aa66c855
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-03 03:24:42 +0530

    Time: 113 ms (63.93%), Space: 44.9 MB (24.81%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,35 +1,39 @@
-#include<vector>
-#include<climits>
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        int dp[n+1][amount+1];
+        int **dp=new int*[n+1];
+        for(int i=0;i<=n;i++){
+            dp[i]=new int[amount+1];
+        }
         
         for(int i=0;i<=n;i++){
-            dp[i][0]=0;
+            for(int j=0;j<=amount;j++){
+                if(j==0){
+                    dp[i][j]=0;
+                }
+                if(i==0){
+                    dp[i][j]=INT_MAX-1;
+                }
+            }
         }
+        
+        int i=1;
         for(int j=1;j<=amount;j++){
-            dp[0][j]=INT_MAX;
+            if(j%coins[i-1]==0){
+                dp[i][j]=j/coins[i-1];
+            }
+            else{
+                dp[i][j]=INT_MAX-1;
+            }
         }
-//         for(int j=1;j<=amount;j++){
-//             if(j%coins[0]==0){
-//                 dp[1][j]=j/coins[0];
-//             }
-//             else{
-//                 dp[1][j]=INT_MAX-1;
-//             }
-            
-//         }
-        for(int i=1;i<=n;i++){
+        
+        for(int i=2;i<=n;i++){
             for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
-                    int x=dp[i-1][j];
-                    int y=dp[i][j-coins[i-1]];
-                    if(y!=INT_MAX){
-                        y+=1;
-                    }
-                    dp[i][j]=min(x,y);
+                    int option1=1+dp[i][j-coins[i-1]];
+                    int option2=dp[i-1][j];
+                    dp[i][j]=min(option1,option2);
                 }
                 else{
                     dp[i][j]=dp[i-1][j];
@@ -37,9 +41,9 @@ public:
             }
         }
         
-       if(dp[n][amount]==INT_MAX){
-           return -1;
-       }
+        if(dp[n][amount]==INT_MAX-1){
+            return -1;
+        }
         return dp[n][amount];
         
     }
