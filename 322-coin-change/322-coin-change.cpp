70aaba9fd073e29cbd77d445fc34fe73e27a6267
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-03 03:09:14 +0530

    Time: 76 ms (89.06%), Space: 10.5 MB (88.57%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -5,25 +5,30 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         int dp[n+1][amount+1];
+        
         for(int i=0;i<=n;i++){
             dp[i][0]=0;
         }
         for(int j=1;j<=amount;j++){
-            dp[0][j]=INT_MAX-1;
+            dp[0][j]=INT_MAX;
         }
-        for(int j=1;j<=amount;j++){
-            if(j%coins[0]==0){
-                dp[1][j]=j/coins[0];
-            }
-            else{
-                dp[1][j]=INT_MAX-1;
-            }
-        }
-        for(int i=2;i<=n;i++){
+//         for(int j=1;j<=amount;j++){
+//             if(j%coins[0]==0){
+//                 dp[1][j]=j/coins[0];
+//             }
+//             else{
+//                 dp[1][j]=INT_MAX-1;
+//             }
+            
+//         }
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
                     int x=dp[i-1][j];
-                    int y=1+dp[i][j-coins[i-1]];
+                    int y=dp[i][j-coins[i-1]];
+                    if(y!=INT_MAX){
+                        y+=1;
+                    }
                     dp[i][j]=min(x,y);
                 }
                 else{
@@ -32,9 +37,9 @@ public:
             }
         }
         
-        if(dp[n][amount]==INT_MAX-1 || dp[n][amount]==INT_MAX){
-            return -1;
-        }
+       if(dp[n][amount]==INT_MAX){
+           return -1;
+       }
         return dp[n][amount];
         
     }
