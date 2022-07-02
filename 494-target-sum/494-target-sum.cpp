a4dfddceebb437090204e6ae767dbde0468f890b
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-03 00:41:34 +0530

    Time: 1535 ms (12.92%), Space: 8.9 MB (93.79%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    int countSubsetSum(vector<int>& nums, int subsetSum){
-        int n =nums.size();
-        int ** dp=new int*[nums.size()+1];
-        for(int i=0;i<=nums.size();i++){
-            dp[i]=new int[subsetSum+1];
-        }
-        
-       for(int i=0;i<=nums.size();i++){
-            dp[i][0]=1;
-        }
-        for(int j=1;j<=subsetSum;j++){
-            dp[0][j]=0;
-        }
-        
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=subsetSum;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+    int helper(vector<int>&nums,int target,int n){
+        if(n==0){
+            if(target==0){
+                return 1;
             }
-        }
-        return dp[n][subsetSum];
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-         int totalSum=0;
-        for(int i=0;i<nums.size();i++){
-            totalSum+=nums[i];
-        }
-        if(target>totalSum){
             return 0;
         }
-        int subset_Sum=(totalSum+target)/2;
-        if(subset_Sum<0 ||(totalSum+target)%2!=0){
-           return 0;
-        }
-        int ans=countSubsetSum(nums,subset_Sum);
-        return ans;
+        
+        // we cant consider an option for target==0 as we did in sum problem becoz here we have to consider all elements ,we cant pick a sequence from it.
+        int option1=helper(nums,target-nums[n-1],n-1);
+        int option2=helper(nums,target+nums[n-1],n-1);
+        
+        return option1+option2;
+        
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+      return helper(nums,target,nums.size());
+        
     }
 };
