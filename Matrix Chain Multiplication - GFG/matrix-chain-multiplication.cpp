299605d29ac720dc69cb67b38a1b7ffab9ef33ae
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-04 00:32:42 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,37 +9,35 @@ using namespace std;
 
 class Solution{
 public:
-    int helper(int N,int arr[],int i,int j,int **dp){
+    int MCM(int arr[],int **dp,int i,int j){
         if(i>=j){
             return 0;
         }
-        
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int ans=INT_MAX;
-        for(int k=i;k<=j-1;k++){
-            int temp=helper(N,arr,i,k,dp)+helper(N,arr,k+1,j,dp)+arr[i-1]*arr[k]*arr[j];
-            if(temp<ans){
-                ans=temp;
+        int mn=INT_MAX;
+        for(int k=i;k<j;k++){
+            int tempAns=MCM(arr,dp,i,k)+MCM(arr,dp,k+1,j)+arr[i-1]* arr[k]* arr[j];
+            if(tempAns<mn){
+                mn=tempAns;
             }
         }
-        return dp[i][j]=ans;
+        return dp[i][j]=mn;
     }
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-    
-        int **dp=new int*[N];
-        for(int i=0;i<N;i++){
-            dp[i]=new int[N];
-            for(int j=0;j<N;j++){
+        int **dp=new int*[N+1];
+        for(int i=0;i<=N;i++){
+            dp[i]=new int[N+1];
+            for(int j=0;j<=N;j++){
                 dp[i][j]=-1;
             }
         }
         
-        int ans=helper(N,arr,1,N-1,dp);
-        return ans;
+        return MCM(arr,dp,1,N-1);
+        
         
     }
 };
