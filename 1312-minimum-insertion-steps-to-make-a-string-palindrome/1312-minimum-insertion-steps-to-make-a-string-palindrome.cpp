0706542b86caa5eb6bc68336466ea9869d3b7b91
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-03 15:52:38 +0530

    Time: 73 ms (50.03%), Space: 7.4 MB (83.31%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,32 +1,37 @@
 #include<string>
+#include<algorithm>
 class Solution {
 public:
-    int minInsertions(string s) {
-      string s1;
-      for(int i=s.size()-1;i>=0;i--){
-          s1.push_back(s[i]);
-      }
-        int n=s.size();
-        int dp[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-            }
+     int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size();
+        int ans[m+1][n+1];
+        for(int i=0;i<=m;i++){
+           ans[i][0]=0;
+        }
+        for(int j=1;j<=n;j++){
+            ans[0][j]=0;
         }
         
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(s[i-1]==s1[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                if(text1[i-1]==text2[j-1]){
+                    ans[i][j]=1+ans[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    int x=ans[i-1][j];
+                    int y=ans[i][j-1];
+                    ans[i][j]=max(x,y);
                 }
             }
         }
-        int ans=s.size()-dp[n][n];
-        return ans;
+        return ans[m][n];
+     }
+    int minInsertions(string s) {
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+        int lcs=longestCommonSubsequence(s, s1);
+        return s.size()-lcs;
+        
     }
 };
