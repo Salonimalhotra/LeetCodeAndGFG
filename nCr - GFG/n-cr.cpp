4f89edb11c8446d243733b2c949571c01d991a20
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-06 21:22:20 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -12,31 +12,33 @@ public:
     int nCr(int n, int r){
         // code here
         int mod=pow(10,9)+7;
-        int dp[n+1][r+1];
+        int **dp=new int*[n+1];
+        for(int i=0;i<=n;i++){
+            dp[i]=new int[r+1];
+            
+        }
+        
         for(int i=0;i<=n;i++){
             for(int j=0;j<=r;j++){
-                if(i==0 && j==0){
-                    dp[0][0]=1;
+                if(i==0){
+                    dp[i][j]=0;
                 }
-                else if(i==0 && j!=0){
-                    dp[0][j]=0;
-                }
-                else if(i!=0 && j==0){
-                    dp[i][0]=1;
+                else if(j==0){
+                    dp[i][j]=1;
                 }
             }
         }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=r;j++){
-                if(i>j){
-                    dp[i][j]=((dp[i-1][j-1])%mod+(dp[i-1][j])%mod)%mod;
+                if(j>i){
+                    dp[i][j]=0;
                 }
                 else if(i==j){
                     dp[i][j]=1;
                 }
-                else if(i<j){
-                    dp[i][j]=0;
+                else{
+                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%mod;
                 }
             }
         }
