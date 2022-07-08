333316c2ebce00fc6970076092ce84181cc426a5
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-08 12:05:01 +0530

    Time: 27 ms (63.15%), Space: 16.5 MB (81.32%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -3,27 +3,28 @@ class Solution {
 public:
     int jump(vector<int>& arr) {
         int n=arr.size();
-        if(n<=1){
+        if(arr.size()<=1){
             return 0;
         }
-        if(arr[0]==0){
-            return -1;
-        }
-        int * dp=new int [n];
-        dp[0]=0;
+        int maxreach=arr[0];
+        int steps=arr[0];
+        int jumps=1;
         for(int i=1;i<n;i++){
-            dp[i]=INT_MAX;
-           for(int j=0;j<i;j++){
-               if(dp[j]!=INT_MAX && arr[j]+j>=i){
-                   if(dp[j]+1<dp[i]){
-                       dp[i]=dp[j]+1;
+            if(i==n-1){
+                return jumps;
+            }
+            steps--;
+            maxreach=max(maxreach,arr[i]+i);
+            
+            if(steps==0){
+                jumps++;
+                if(i>=maxreach){
+                    return -1;
                 }
-             }
-          }
+                steps=maxreach-i;
+            }
         }
-        if(dp[n-1]==INT_MAX){
-            return -1;
-        }
-        return dp[n-1];
-    }
+        
+        return jumps;
+    }   
 };
