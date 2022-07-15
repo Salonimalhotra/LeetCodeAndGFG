4dd0b24954d64d470c00892ed8db851e940e39a2
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-07-15 21:37:59 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,29 +5,45 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 public:
-   bool bfs(int vertex,int V,int * colours,vector<int>adj[]){
-       colours[vertex]=0;
-       queue<int>pending;
-       pending.push(vertex);
-       while(pending.size()!=0){
-           int top=pending.front();
-           pending.pop();
-           for(auto i:adj[top]){
-               if(colours[i]!=-1){
-                   if(colours[i]==colours[top]){
-                       return false;
-                   }
-                   else{
-                       continue;
-                   }
-               }
-               else{
-                   colours[i]=1-colours[top];
-                   pending.push(i);
-               }
-           }
-       }
-       return true;
+   bool dfs(int vertex,int V,int * colours,vector<int>adj[]){
+    //   colours[vertex]=0;
+    //   queue<int>pending;
+    //   pending.push(vertex);
+    //   while(pending.size()!=0){
+    //       int top=pending.front();
+    //       pending.pop();
+    //       for(auto i:adj[top]){
+    //           if(colours[i]!=-1){
+    //               if(colours[i]==colours[top]){
+    //                   return false;
+    //               }
+    //               else{
+    //                   continue;
+    //               }
+    //           }
+    //           else{
+    //               colours[i]=1-colours[top];
+    //               pending.push(i);
+    //           }
+    //       }
+    //   }
+    //return true;
+      for(auto i:adj[vertex]){
+          if(colours[i]!=-1){
+              if(colours[i]==colours[vertex]){
+                  return false;
+              }
+          }
+          else{
+              colours[i]=1-colours[vertex];
+              bool ans=dfs(i,V,colours,adj);
+              if(!ans){
+                  return false;
+              }
+          }
+      }
+    return true;    
+       
    }
 	bool isBipartite(int V, vector<int>adj[]){
 	    // Code here
@@ -38,7 +54,8 @@ public:
 	    
 	    for(int i=0;i<V;i++){
 	        if(colours[i]==-1){
-	            if(!bfs(i,V,colours,adj)){
+	            colours[i]=1;
+	            if(!dfs(i,V,colours,adj)){
 	                return false;
 	            }
 	        }
