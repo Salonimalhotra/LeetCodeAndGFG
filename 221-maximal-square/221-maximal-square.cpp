dd8ccaff54f2d9adae091c3727b74aba53235007
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-04 23:16:28 +0530

    Time: 116 ms (40.50%), Space: 18.2 MB (39.60%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-    int  solve(vector<vector<char>>& matrix,int i,int j,int &maxi,int **dp){
-        if(i>=matrix.size() || j>=matrix[0].size()){
-            return 0 ;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int down=solve(matrix,i+1,j,maxi,dp);
-        int right=solve(matrix,i,j+1,maxi,dp);
-        int digonal=solve(matrix,i+1,j+1,maxi,dp);
-        
-        if(matrix[i][j]=='1'){
-            int smallAns=1+ min(down,min(right, digonal));
-            maxi=max(maxi,smallAns);
-            return dp[i][j]=smallAns;
-        }
-        else{
-            return dp[i][j]=0;
-        }       
-        
-    }
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int maxi=0;
-        int i=0;
-        int j=0;
+    int helperDP(vector<vector<char>>&matrix,int &maxi){
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         
-        int **dp=new int*[n+1];
-        for(int i=0;i<=n;i++){
-           dp[i]=new int[m+1];
-            for(int j=0;j<=m;j++){
-                dp[i][j]=-1;
-            }
-        }
-        
-        int ans=solve(matrix,i,j,maxi,dp);
-        return maxi * maxi;       
-        
+       for(int i=n-1;i>=0;i--){
+           for(int j=m-1;j>=0;j--){
+               if(matrix[i][j]=='1'){
+                   dp[i][j]=1 + min(dp[i+1][j],min(dp[i][j+1],dp[i+1][j+1]));
+                   maxi=max(maxi,dp[i][j]);
+               }
+               else{
+                   dp[i][j]=0;
+               }
+           }
+       }
+        return dp[0][0];
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+         int maxi=0;
+         int smallAns=helperDP(matrix,maxi);
+        return maxi * maxi;
     }
 };
