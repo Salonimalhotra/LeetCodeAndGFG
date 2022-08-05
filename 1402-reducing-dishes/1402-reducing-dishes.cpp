15d705953e1dc3ce14dc7bd3223ccf47fb4813de
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-05 21:47:08 +0530

    Time: 32 ms (52.23%), Space: 8.3 MB (58.70%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -6,25 +6,27 @@ public:
         int n=satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
         
-        // vector<int>next(n+1,0);
-        // vector<int>curr(n+1,0);
-        int **dp=new int *[n+1];
-        for(int i=0;i<=n;i++){
-            dp[i]=new int[n+1];
-        }
-        for(int j=0;j<=n;j++){
-            dp[n][j]=0;
-        }
+        vector<int>next(n+1,0);
+        vector<int>curr(n+1,0);
+        // int **dp=new int *[n+1];
+        // for(int i=0;i<=n;i++){
+        //     dp[i]=new int[n+1];
+        // }
+        // for(int j=0;j<=n;j++){
+        //     dp[n][j]=0;
+        // }
         for(int index=n-1;index>=0;index--){
-            // we are starting timer from index coz we know that if we are at index 3 then we csnt have value of timer to be greater than 3;
-            for(int timer=n-1;timer>=0;timer--){
-                int incl=satisfaction[index] * (timer+1) + dp[index+1][timer+1];
-                int excl=dp[index+1][timer];
-                dp[index][timer]=max(incl,excl);
+            for(int timer=index;timer>=0;timer--){
+            // // we are starting timer from index coz we know that if we are at index 3 then we csnt have value of timer to be greater than 3;
+            // for(int timer=n-1;timer>=0;timer--){ // this is also valid but there is no need for it coz we know at index 3 we can have maximum 4 as a timer but not more than that
+                int incl=satisfaction[index] * (timer+1) + next[timer+1];
+                int excl=next[timer];
+                curr[timer]=max(incl,excl);
             }
+            next=curr;
         }
         
-        return dp[0][0];
+        return next[0];
         
        
     }
