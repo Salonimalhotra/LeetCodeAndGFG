04bed79f182551e1188a44b22e7f0e4aba11f113
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-05 21:44:20 +0530

    Time: 35 ms (50.51%), Space: 20.7 MB (38.59%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -5,6 +5,9 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n=satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
+        
+        // vector<int>next(n+1,0);
+        // vector<int>curr(n+1,0);
         int **dp=new int *[n+1];
         for(int i=0;i<=n;i++){
             dp[i]=new int[n+1];
@@ -13,7 +16,8 @@ public:
             dp[n][j]=0;
         }
         for(int index=n-1;index>=0;index--){
-            for(int timer=index;timer>=0;timer--){
+            // we are starting timer from index coz we know that if we are at index 3 then we csnt have value of timer to be greater than 3;
+            for(int timer=n-1;timer>=0;timer--){
                 int incl=satisfaction[index] * (timer+1) + dp[index+1][timer+1];
                 int excl=dp[index+1][timer];
                 dp[index][timer]=max(incl,excl);
