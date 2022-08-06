36ecf1f539ade7518b23f7cbb295a3d8444797bf
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-06 08:37:00 +0530

    Time: 404 ms (93.11%), Space: 77.5 MB (66.73%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -18,22 +18,7 @@ public:
             return false;
         }
     }
-    // int LIS(vector<vector<int>>& envelopes){
-    //     vector<int>ans;
-    //     ans.push_back(envelopes[0][1]);
-    //     int len=1;
-    //     for(int i=1;i<envelopes.size();i++){
-    //         if(envelopes[i][1]>ans.back()){
-    //             ans.push_back(envelopes[i][1]);
-    //             len++;
-    //         }
-    //         else{
-    //             int idx=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
-    //             ans[idx]=envelopes[i][1];
-    //         }
-    //     }
-    //     return len;
-    // }
+   
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(),envelopes.end(),compare);
          vector<int>ans;
