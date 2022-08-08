880f9b6a3890e80556b3d438ab887845c67ff5dd
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-08 06:41:17 +0530

    Time: 434 ms (28.78%), Space: 120.4 MB (37.71%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,41 +1,28 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    int solve(vector<int>&nums1,vector<int>&nums2,int index,bool swapped,vector<vector<int>>&dp){
-        if(index==nums1.size()){
-            return 0;
-        }
-        
-        if(dp[index][swapped]!=-1){
-            return dp[index][swapped];
-        }
-        
-        int prev1=nums1[index-1];
-        int prev2=nums2[index-1];
-        
-        if(swapped){
-            swap(prev1,prev2);
-        }
-        
-        int ans=INT_MAX;
-        
-        //prev1 and prev2 are not swapped and we arent swapping our elements at respective indexes 
-        if(nums1[index]>prev1 && nums2[index]>prev2){
-            ans=min(ans,solve(nums1,nums2,index+1,0,dp));
-        }
-        
-        if(nums1[index]>prev2 && nums2[index]>prev1){
-               ans=min(ans,1+solve(nums1,nums2,index+1,1,dp));
-                  
-        }
-        
-        return dp[index][swapped]=ans;        
-    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        // bottom up DP
         nums1.insert(nums1.begin(),-1);
         nums2.insert(nums2.begin(),-1);
-        bool swapped=0;
-        vector<vector<int>>dp(nums1.size(),vector<int>(2,-1));
-        return solve(nums1,nums2,1,swapped,dp);        
+        int n=nums1.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        for(int index=n-1;index>=1;index--){
+            for(int swapped=1;swapped>=0;swapped--){
+                int prev1=nums1[index-1];
+                int prev2=nums2[index-1];
+                if(swapped){
+                    swap(prev1,prev2);
+                }
+                int ans=INT_MAX;
+                if(nums1[index]>prev1 && nums2[index]>prev2){
+                    ans=min(ans,dp[index+1][0]);
+                }
+                if(nums1[index]>prev2 && nums2[index]>prev1){
+                    ans=min(ans,1+dp[index+1][1]);
+                }
+                dp[index][swapped]=ans;
+            }
+        }
+        return dp[1][0];
     }
 };
