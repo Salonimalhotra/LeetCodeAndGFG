8feffa4534888711f518f4542204b95038e39fac
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-08 17:50:53 +0530

    Time: 7 ms (79.55%), Space: 8.8 MB (50.36%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,27 +3,31 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int rows=triangle.size();
         int cols=triangle[rows-1].size();
-        int **dp=new int*[rows];
-        for(int i=0;i<rows;i++){
-            dp[i]=new int[cols];
-            for(int j=0;j<cols;j++){
-                dp[i][j]=INT_MAX;
-            }
-        }
+        // int **dp=new int*[rows];
+        // for(int i=0;i<rows;i++){
+        //     dp[i]=new int[cols];
+        //     for(int j=0;j<cols;j++){
+        //         dp[i][j]=INT_MAX;
+        //     }
+        // }
+        vector<int>prev(cols,0);
+        vector<int>curr(cols,0);
+        
         
         for(int j=0;j<cols;j++){
-            dp[rows-1][j]=triangle[rows-1][j];
+            prev[j]=triangle[rows-1][j];
         }
         
         int col=cols-1;
         for(int i=rows-2;i>=0;i--){
             for(int j=0;j<col;j++){
-                dp[i][j]=min(dp[i+1][j],dp[i+1][j+1]) + triangle[i][j];
+                curr[j]=min(prev[j],prev[j+1]) + triangle[i][j];
             }
+            prev=curr;
             col--;
         }
         
-        return dp[0][0];
+        return prev[0];
         
     }
 };
