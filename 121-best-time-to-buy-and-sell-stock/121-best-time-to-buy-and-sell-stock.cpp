ece5eb2f723003d953c740ebf65d69fde52520b1
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-09 18:11:56 +0530

    Time: 178 ms (67.32%), Space: 93.4 MB (52.47%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -15,15 +15,25 @@ public:
 //         return maxProfit;
         
         // second Approach - O{n} -time complexity and O{n}- space complexity
-        int minArray[prices.size()];
-        minArray[0]=prices[0];
-        int profit=0;
-        for(int j=1;j<prices.size();j++){
-            minArray[j]=min(minArray[j-1],prices[j]);
-        }
+//         int minArray[prices.size()];
+//         minArray[0]=prices[0];
+//         int profit=0;
+//         for(int j=1;j<prices.size();j++){
+//             minArray[j]=min(minArray[j-1],prices[j]);
+//         }
         
+//         for(int i=1;i<prices.size();i++){
+//             profit=max(profit,prices[i]-minArray[i]);
+//         }
+//         return profit;
+        
+        // third Approach - O(n)time  and O{1} - space
+        int profit=0;
+        int mini=prices[0];
         for(int i=1;i<prices.size();i++){
-            profit=max(profit,prices[i]-minArray[i]);
+            int cost=prices[i]- mini;
+            profit=max(profit,cost);
+            mini=min(mini,prices[i]);
         }
         return profit;
     }
