194722ef1bbfe530f86f1dc12223ecfb0a36b72f
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-09 18:06:55 +0530

    Time: 150 ms (86.69%), Space: 93.6 MB (8.81%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,30 @@
-#include<vector>
+#include<bits/stdc++.h>
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int suffixMax[prices.size()];
-        suffixMax[n-1]=prices[n-1];
-        for(int j=n-2;j>=0;j--){
-            suffixMax[j]=max(suffixMax[j+1],prices[j]);
+        // first Approach - O{n^2} - time complexity
+//         int maxProfit=0;
+//         for(int i=0;i<prices.size();i++){
+//             int mini=INT_MAX;
+//             for(int j=i-1;j>=0;j--){
+//                 mini=min(prices[j],mini);
+//             }
+//             maxProfit=max(maxProfit,prices[i]-mini);
+//         }
+        
+//         return maxProfit;
+        
+        // second Approach - O{n} -time complexity and O{n}- space complexity
+        int minArray[prices.size()];
+        minArray[0]=prices[0];
+        int profit=0;
+        for(int j=1;j<prices.size();j++){
+            minArray[j]=min(minArray[j-1],prices[j]);
         }
         
-        int max_profit=0;
-        for(int i=0;i<prices.size();i++){
-            int profit=suffixMax[i]-prices[i];
-            max_profit=max(max_profit,profit);
+        for(int i=1;i<prices.size();i++){
+            profit=max(profit,prices[i]-minArray[i-1]);
         }
-        return max_profit;
-        
+        return profit;
     }
 };
