d7c377eff2dd399c58e8d6cb47a932266726467c
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-09 17:25:32 +0530

    Time: 45 ms (87.04%), Space: 7.2 MB (87.81%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -40,12 +40,13 @@ public:
     bool Tab(string s,string p){
               int n=s.size();
               int m=p.size();
-             vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-             dp[0][0]=1;
+            vector<int>prev(m+1,0);
+            vector<int>curr(m+1,0);
+             prev[0]=1;
         
-             for(int i=1;i<=n;i++){
-                 dp[i][0]=0;
-             }
+//              for(int i=1;i<=n;i++){
+//                  dp[i][0]=0;
+//              }
         
             for(int j=1;j<=m;j++){
                 int flag=1;
@@ -55,23 +56,24 @@ public:
                     break;
                   }                    
                  }
-                 dp[0][j]=flag;             
+                prev[j]=flag;             
             }
         
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=m;j++){
                     if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                        dp[i][j]=dp[i-1][j-1];
+                        curr[j]=prev[j-1];
                     }
                     else if(p[j-1]=='*'){
-                        dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                        curr[j]=prev[j] || curr[j-1];
                     }
                     else{
-                        dp[i][j]=0;
+                        curr[j]=0;
                     }
                 }
+                prev=curr;
             }
-        return dp[n][m];
+        return prev[m];
     }
     
     bool isMatch(string s, string p) {
