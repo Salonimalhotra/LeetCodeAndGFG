503aa628f9ab617f67a3036ae51a94ab4894b98e
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-09 21:53:52 +0530

    Time: 6 ms (67.48%), Space: 11.9 MB (47.15%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,28 +2,28 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    int solve(vector<int>&prices,int index,int prevBuy,vector<vector<int>>&dp){
-        if(index>=prices.size()){
-            return 0;
-        }
-        if(dp[index][prevBuy]!=-1){
-            return dp[index][prevBuy];
-        }
-        int option1;
-        int option2;
-        if(prevBuy==1){
-            // we have previously bought now we can only sell
-            option1=prices[index] + solve(prices,index+2,0,dp);
-            option2=solve(prices,index+1,1,dp);
-        }
+//     int solve(vector<int>&prices,int index,int prevBuy,vector<vector<int>>&dp){
+//         if(index>=prices.size()){
+//             return 0;
+//         }
+//         if(dp[index][prevBuy]!=-1){
+//             return dp[index][prevBuy];
+//         }
+//         int option1;
+//         int option2;
+//         if(prevBuy==1){
+//             // we have previously bought now we can only sell
+//             option1=prices[index] + solve(prices,index+2,0,dp);
+//             option2=solve(prices,index+1,1,dp);
+//         }
         
-        else{
-            option1=-1 * prices[index] + solve(prices,index+1,1,dp);
-            option2=solve(prices,index+1,0,dp);
-        }
+//         else{
+//             option1=-1 * prices[index] + solve(prices,index+1,1,dp);
+//             option2=solve(prices,index+1,0,dp);
+//         }
         
-        return dp[index][prevBuy]=max(option1,option2);
-    }
+//         return dp[index][prevBuy]=max(option1,option2);
+//     }
     
     int tab(vector<int>prices){
         int n=prices.size();
@@ -51,7 +51,8 @@ public:
         return dp[0][0];
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size()+2,vector<int>(2,-1));
-        return solve(prices,0,0,dp);
+        // vector<vector<int>>dp(prices.size()+2,vector<int>(2,-1));
+        // return solve(prices,0,0,dp);
+        return tab(prices);
     }
 };
