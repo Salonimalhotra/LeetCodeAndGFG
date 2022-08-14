b14a9f5147231a20f7e2d22e86e403f51c3a6040
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-14 22:30:16 +0530

    Time: 29 ms (47.58%), Space: 14.6 MB (69.38%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        stack<int>s;
+//         stack<int>s;
         
-        s.push(arr[arr.size()-1]);
+//         s.push(arr[arr.size()-1]);
+//         arr[arr.size()-1]=-1;
+//         int i=arr.size()-2;
+//         while(i>=0){
+//             if(s.size()!=0){
+//                 int topElement=s.top();
+//                 if(arr[i]>topElement){
+//                     s.push(arr[i]);
+//                 }
+//                 arr[i]=topElement;
+//             }
+//             i--;
+//         }
+        
+//         return arr;
+        
+        int maxElement=arr[arr.size()-1];
         arr[arr.size()-1]=-1;
-        int i=arr.size()-2;
-        while(i>=0){
-            if(s.size()!=0){
-                int topElement=s.top();
-                if(arr[i]>topElement){
-                    s.push(arr[i]);
-                }
-                arr[i]=topElement;
+        for(int i=arr.size()-2;i>=0;i--){
+            int curr=arr[i];
+            arr[i]=maxElement;
+            if(curr>maxElement){
+                maxElement=curr;
             }
-            i--;
         }
-        
         return arr;
     }
 };
