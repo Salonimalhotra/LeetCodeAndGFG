88cc0653c52fe28ce9c384eb57def7e065650c05
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-15 19:05:57 +0530

    Time: 7 ms (6.24%), Space: 6.4 MB (28.62%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n<=1){
+            return n;
+        }
+        int maxAns=INT_MIN;
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
+            }
+            else{
+                dp[i]=dp[(i-1)/2] + dp[(i-1)/2 +1];
+            }
+            cout<<dp[i]<<endl;
+            maxAns=max(dp[i],maxAns);
+        }
+        return maxAns;
+        
+    }
+};
