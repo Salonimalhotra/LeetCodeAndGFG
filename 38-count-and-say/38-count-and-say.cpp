b119c1c0386d3e772d99256d5c444a50b6ae8d82
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-19 22:17:56 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (79.38%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,21 +1,25 @@
-#include<unordered_map>
 class Solution {
 public:
-  string countAndSay(int n) {
-        if(n==1) return "1";
-        string s=countAndSay(n-1);
-        string res;
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        string smallAns=countAndSay(n-1);
+        string finalAns="";
         int cnt=1;
-        for(int i=0;i<s.length();i++){
-            if(i==s.size()-1||s[i]!=s[i+1]){
-                res+=to_string(cnt);
-                res+=s[i];
+        int i=0;
+        while(i<smallAns.size()){           
+            if(i==smallAns.size()-1 || smallAns[i]!=smallAns[i+1]){
+                finalAns.push_back('0' + cnt);
+                finalAns.push_back(smallAns[i]);
                 cnt=1;
-            }
-            else{
+                i++;
+            }            
+             else if(smallAns[i]==smallAns[i+1]){
+                i++;
                 cnt++;
             }
         }
-        return res;
+        return finalAns;
     }
 };
