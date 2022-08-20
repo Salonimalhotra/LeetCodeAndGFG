031b0a26dc998bb8f7847ce0eb3ae4ca70cf9edc
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-20 23:11:58 +0530

    Time: 4 ms (34.28%), Space: 6.3 MB (58.27%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                if(st.size()==0 || (st.size()!=0 && st.top()==')')){
+                   st.push(s[i]);
+                }
+                else if(st.top()=='('){
+                    st.pop();
+                }
+            }
+        }
+        return st.size();
+    }
+};
