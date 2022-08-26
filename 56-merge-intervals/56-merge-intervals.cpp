1dc0f26c8bdee857970ad5d192c3fd229c07aee4
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-26 23:33:21 +0530

    Time: 27 ms (99.87%), Space: 20.7 MB (11.91%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,30 +1,52 @@
-#include<vector>
-#include<bits/stdc++.h>
-#include<stack>
 class Solution {
 public:
+//     bool static cmp(pair<int,int>p1,pair<int,int>p2){
+//         if(p1.first<p2.first){
+//             return true;
+//         }
+//         else if(p1.first==p2.first){
+//             if(p1.second<=p2.second){
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+            
+//         }
+//         else{
+//             return false;
+//         }
+//     }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        // vector<vector<int>>ans;
-        stack<vector<int>>s;
-        s.push(intervals[0]);
-        int i=1;
-        while(i<intervals.size()){
-          if(s.top()[1]>=intervals[i][0]){
-              s.top()[1]=max(intervals[i][1],s.top()[1]);
-              i++;
-          }
+        vector<pair<int,int>>v;
+        for(int i=0;i<intervals.size();i++){
+            pair<int,int>p;
+            p.first=intervals[i][0];
+            p.second=intervals[i][1];
+            v.push_back(p);
+        }
+        
+        sort(v.begin(),v.end());
+        vector<vector<int>>ans;
+        vector<int>v1;
+        v1.push_back(v[0].first);
+        v1.push_back(v[0].second);
+        ans.push_back(v1);
+        int last=0;
+        // cout<<v1[0]<<endl;
+        // cout<<v1[1]<<endl;
+        for(int i=1;i<v.size();i++){
+            if(ans[last][1]>=v[i].first){
+                ans[last][1]=max(v[i].second,ans[last][1]);
+            }
             else{
-                s.push(intervals[i]);
-                i++;
+                vector<int>temp;
+                temp.push_back(v[i].first);
+                temp.push_back(v[i].second);
+                ans.push_back(temp);
+                last++;
             }
         }
-        intervals.clear();
-        while(s.size()!=0){
-            intervals.push_back(s.top());
-            s.pop();
-        }
-        return  intervals;
-        
+        return ans;
     }
 };
