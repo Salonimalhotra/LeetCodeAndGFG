ce32885964c6eaa732b3d543e39e7a6682919be9
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-28 22:40:43 +0530

    Time: 590 ms (70.55%), Space: 178.6 MB (24.64%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,73 +1,82 @@
- class node {
-      public:
-        int key;
-      int val;
-      node * next;
-      node * prev;
-      node(int _key, int _val) {
-        key = _key;
-        val = _val;
-      }
-    };
-
+class Node{
+    public:
+    int key;
+    int value;
+    Node * prev;
+    Node * next;
+    
+    Node(int k,int v){
+        this->key=k;
+        this->value=v;
+        prev=NULL;
+        next=NULL;
+    }
+};
 class LRUCache {
-  public:
-   
-  node * head = new node(-1, -1);
-  node * tail = new node(-1, -1);
-
-  int cap;
-  unordered_map < int, node * > m;
-
-  LRUCache(int capacity) {
-    cap = capacity;
-    head -> next = tail;
-    tail -> prev = head;
-  }
-
-  void addnode(node * newnode) {
-    node * temp = head -> next;
-    newnode -> next = temp;
-    newnode -> prev = head;
-    head -> next = newnode;
-    temp -> prev = newnode;
-  }
-
-  void deletenode(node * delnode) {
-    node * delprev = delnode -> prev;
-    node * delnext = delnode -> next;
-    delprev -> next = delnext;
-    delnext -> prev = delprev;
-  }
-
-  int get(int key_) {
-    if (m.find(key_) != m.end()) {
-      node * resnode = m[key_];
-      int res = resnode -> val;
-      m.erase(key_);
-      deletenode(resnode);
-      addnode(resnode);
-      m[key_] = head -> next;
-      return res;
+public:
+    Node * head;
+    Node * tail;
+    int capacity;
+    unordered_map<int,Node*>mp;
+    LRUCache(int capacity) {
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+        head->next=tail;
+        tail->prev=head;
+        this->capacity=capacity;
+        
     }
-
-    return -1;
-  }
-
-  void put(int key_, int value) {
-    if (m.find(key_) != m.end()) {
-      node * existingnode = m[key_];
-      m.erase(key_);
-      deletenode(existingnode);
+    
+    void deleteNode(Node * delnode){
+        Node * delprev=delnode->prev;
+        Node * delnext=delnode->next;
+        
+        delprev->next=delnext;
+        delnext->prev=delprev;
+        
     }
-    if (m.size() == cap) {
-      m.erase(tail -> prev -> key);
-      deletenode(tail -> prev);
+    
+    void addNode(Node * newNode){
+    Node * temp = head -> next;
+    newNode -> next = temp;
+    newNode -> prev = head;
+    head -> next = newNode;
+    temp -> prev = newNode;
+        return;
     }
-
-    addnode(new node(key_, value));
-    m[key_] = head -> next;
-  }
+    int get(int key) {
+        if(mp.find(key)!=mp.end()){
+           Node * temp=mp[key];
+           int res=mp[key]->value;
+           mp.erase(key);
+           deleteNode(temp);
+           // Node * newnode=new Node(key,res);
+           addNode(temp);
+           mp[key]=head->next;
+           return res;
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            Node * delnode=mp[key];
+            mp.erase(key);
+            deleteNode(delnode);
+        }
+        
+        if(mp.size()==capacity){
+            mp.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+        
+        Node * newNode=new Node(key,value);
+        addNode(newNode);
+        mp[key]=head->next;
+        return;
+    }
 };
 
 /**
