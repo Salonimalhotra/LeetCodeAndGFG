ceb3930b4e45dfb2b73d662d2695b46d55c4f70b
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-09-01 03:55:39 +0530

    Time: 394 ms (43.09%), Space: 137.8 MB (6.11%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -29,44 +29,26 @@ public:
                 break;
             }
             else if(temp->val<prev->val && temp->val<temp->next->val || temp->val>prev->val && temp->val>temp->next->val){
-                 if(mp.size()==0){
-                     mp[pos]++;
-                     prev=temp;
-                     temp=temp->next;
-                     pos++;
-                 }
-//                 else if(mp.size()==1){
-//                     map<int, int>::iterator itr=mp.begin();
-//                      min_p=min(min_p,pos-itr->first);
-//                      max_p=max(max_p,pos-itr->first);
-//                      mp[pos]++;
-//                      prev=temp;
-//                      temp=temp->next;
-//                      pos++;
-                  
-//                 }
-                else{
+                 if(mp.size()!=0){
                     map<int,int>::iterator itr=mp.begin();
                     max_p=max(max_p,pos-itr->first);
                     itr=mp.end();
                     itr--;
                     min_p=min(min_p,pos-itr->first);                  
-                     mp[pos]++;
+                    
+                }
+                    mp[pos]++;
                      prev=temp;
                      temp=temp->next;
                      pos++;
-                }
-            }
+             }
             else{
                      prev=temp;
                      temp=temp->next;
                      pos++;
             }
         }
-//         for(auto i:mp){
-//             cout<<"printing map"<<i.first<<endl;
-//         }
-        
+
         if(min_p==INT_MAX){
             ans[0]=-1;
             ans[1]=-1;
