b31f7a2fe2fa2413b526d041e13804ccd7b6272c
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-08-31 23:32:50 +0530

    Time: 20 ms (74.38%), Space: 11.6 MB (29.77%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -8,63 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Pair{
-    public:
-    ListNode * head;
-    ListNode * tail;
-};
 class Solution {
 public:
-    Pair rev(ListNode * head){
-        if(head==NULL){
-            Pair ans;
-            ans.head=NULL;
-            ans.tail=NULL;
-            return ans;
+    pair<ListNode *,ListNode *> revLinkedlist(ListNode * head){
+        if(head==NULL || head->next==NULL){
+              pair<ListNode *,ListNode *>Ans;
+              Ans.first=head;
+              Ans.second=head;
+              return Ans;
         }
-        else if(head->next==NULL){
-            Pair ans;
-            ans.head=head;
-            ans.tail=head;
-            return ans;
-        }
-           Pair semiAns=rev(head->next);
-           semiAns.tail->next=head;
-           head->next=NULL;
-           Pair finalAns;
-           finalAns.head=semiAns.head;
-           finalAns.tail=head;
-           return finalAns;        
-    }
-    int len(ListNode * head){
-        if(head==NULL){
-            return 0;
-        }
-        return 1+len(head->next);
+        
+          pair<ListNode *,ListNode *> smallAns=revLinkedlist(head->next);
+          smallAns.second->next=head;
+          head->next=NULL;
+          smallAns.second=head;
+          return smallAns;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL || k==0){
-            return head;
-        }
-        else if(k>len(head)){
-            return head;
-        }
-        else if(k==len(head)){
-            Pair ans=rev(head);
-            return ans.head;
-        }
-        int cnt=0;
         ListNode * temp=head;
-        while(temp!=NULL && cnt<k-1){
+        int cnt=1;
+        while(temp!=NULL && cnt<k){
             temp=temp->next;
             cnt++;
         }
-        ListNode * firstTail=temp;
-        ListNode * secondHead=firstTail->next;
-        firstTail->next=NULL;
-        ListNode * semiAns=reverseKGroup(secondHead,k);
-        Pair revAns=rev(head);
-        revAns.tail->next=semiAns;
-        return revAns.head;
+        if(temp==NULL){
+            //  pair<ListNode *,ListNode *> first_k_reverse=revLinkedlist(head);
+            // return first_k_reverse.first;
+            return head;
+        }
+        ListNode * secondhead=temp->next;
+        temp->next=NULL;
+        pair<ListNode *,ListNode *> first_k_reverse=revLinkedlist(head);
+        ListNode * nextAns=reverseKGroup(secondhead,k);
+        first_k_reverse.second->next=nextAns;
+        return first_k_reverse.first;
     }
 };
