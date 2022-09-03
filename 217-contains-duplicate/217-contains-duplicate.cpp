d1ff9a7c23dd0ba8148c1d40c29e961424321540
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-09-03 11:47:33 +0530

    Time: 229 ms (7.28%), Space: 51.4 MB (64.33%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,9 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>1){
-                return true;
+           if(mp.find(nums[i])!=mp.end()){
+               return true;
+           }
+            else{
+                mp[nums[i]]++;
             }
         }
         return false;
