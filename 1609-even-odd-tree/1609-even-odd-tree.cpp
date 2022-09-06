8f1e7d90412cb33065fe6b82c3884ec062e91b4b
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-09-06 11:16:11 +0530

    Time: 532 ms (24.64%), Space: 150.8 MB (79.37%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+          queue<TreeNode *>q;
+          q.push(root);
+        int level=0;
+        while(q.size()!=0){
+            int s=q.size();
+            int lastElement=-1;
+            for(int i=0;i<s;i++){
+                TreeNode * topNode=q.front();
+                q.pop();
+                if(level%2==0){
+                    if(topNode->val%2==0){
+                        return false;
+                    }
+                }
+                
+                if(level%2!=0){
+                    if(topNode->val%2!=0){
+                        return false;
+                    }
+                }
+                
+                if(lastElement==-1){
+                    lastElement=topNode->val;
+                }
+                else{
+                    if(level%2!=0){
+                        if(topNode->val>=lastElement){
+                            return false;
+                        }
+                        else{
+                            lastElement=topNode->val;
+                        }
+                    }
+                    
+                    else{
+                         if(topNode->val<=lastElement){
+                            return false;
+                        }
+                        else{
+                            lastElement=topNode->val;
+                        }
+                    }
+                }
+                
+                if(topNode->left!=NULL){
+                    q.push(topNode->left);
+                }
+                if(topNode->right!=NULL){
+                    q.push(topNode->right);
+                }
+                
+            }
+            level++;
+        }
+        return true;
+    }
+};
