04b8ae5ba922cf0c2e0a2fdfdc75a560b406d56c
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-09-13 18:57:31 +0530

    Time: 0 ms (100.00%), Space: 14.4 MB (74.55%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,66 +11,30 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
         
-        return max(maxDepth(root->left),maxDepth(root->right)) + 1;
+        return max(height(root->left),height(root->right)) + 1;
     }
-    
-    void solve(TreeNode * root,unordered_map<int,int>&mp,int maxD,int lvl){
-        if(root==NULL){
-            return;
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        if(root==NULL){ 
+               return NULL;
         }
         
-        if(lvl==maxD && root!=NULL){
-            mp[root->val]++;
-        }
+        int leftheight=height(root->left);
+        int rightheight=height(root->right);
         
-        solve(root->left,mp,maxD,lvl+1);
-        solve(root->right,mp,maxD,lvl+1);
-        return;
-    }
-    TreeNode * helper(TreeNode * root,unordered_map<int,int>&mp){
-        if(root==NULL){
-            return NULL;
-        }
-        
-        else if(root->left!=NULL && root->right!=NULL){
-            if(mp.find(root->left->val)!=mp.end() && mp.find(root->right->val)!=mp.end()){
-                return root;
-            }           
-            
-        }
-        
-        else if(root->left==NULL && root->right==NULL){
-            if(mp.find(root->val)!=mp.end()){
-                return root;
-            }
-            else{
-                return NULL;
-            }
-        }
-        
-        TreeNode * leftans=helper(root->left,mp);
-        TreeNode * rightans=helper(root->right,mp);
-        if(leftans!=NULL && rightans!=NULL){
+        if(leftheight==rightheight){
             return root;
         }
-        else if(leftans==NULL){
-            return rightans;
-        }     
-        else if(rightans==NULL){
-            return leftans;
+        
+        else if(leftheight>rightheight){
+            return subtreeWithAllDeepest(root->left);
+        }
+        
+        else if(leftheight<rightheight){
+              return subtreeWithAllDeepest(root->right);
         }
         return NULL;
-    }
-      TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        unordered_map<int,int>mp;
-        int maxD=maxDepth(root);
-        solve(root,mp,maxD,1);     
-        return helper(root,mp);
-          
     }
 };
