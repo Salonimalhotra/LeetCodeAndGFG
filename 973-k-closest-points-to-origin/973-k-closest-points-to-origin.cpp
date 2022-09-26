119193090156415256635fd3f64e8243d0b920bc
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-09-26 11:07:16 +0530

    Time: 475 ms (43.48%), Space: 70.6 MB (48.51%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+           priority_queue<pair<double,pair<int,int>>>pq;
+           for(int i=0;i<k;i++){
+               int first_p=points[i][0];
+               int second_p=points[i][1];
+               double distance=sqrt((first_p) * (first_p)  +  (second_p) * (second_p));
+               pq.push(make_pair(distance,make_pair(first_p,second_p)));              
+               
+           }
+        
+          for(int i=k;i<points.size();i++){
+                  int first_p=points[i][0];
+                  int second_p=points[i][1];
+                   double  distance=sqrt((first_p) * (first_p)  +  (second_p) * (second_p));
+              
+                  if(pq.top().first>distance){
+                      pq.pop();
+                      pq.push(make_pair(distance,make_pair(first_p,second_p)));              
+                  }
+          } 
+          vector<vector<int>>ans;
+        
+        while(pq.size()!=0){
+            int firstp=pq.top().second.first;
+            int secondp=pq.top().second.second;
+            pq.pop();
+            vector<int>ans1;
+            ans1.push_back(firstp);
+            ans1.push_back(secondp);
+            ans.push_back(ans1);
+        }
+        
+        return ans;
+          
+    }
+};
