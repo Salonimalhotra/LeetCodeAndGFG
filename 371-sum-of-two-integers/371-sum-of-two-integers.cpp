ea99e7c8d81d5c965b398ee355b1e688b3a03d28
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-01 08:18:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.71%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -5,29 +5,32 @@ public:
         int carry=0;
         for(int i=0;i<32;i++){
              int m= 1<<i;
+          // dealing when both bits of this number are 1 
+          //then also we deal with 2 cases :- one is where we have an carry and second where there is no carry
              if((a & m)!=0 && (b&m)!=0){
                if(carry==0){
                     num= num & ~(m);
                     carry=1;    
                }
-               else if(carry==1){
-                   num= num | m;
-               }             
+               else if(carry==1)num= num | m;                           
              }
+          
+            // dealing when only one of the  bits of this number are 1 
+          //then also we deal with 2 cases :- one is where we have an carry and second where there is no carry
             else if((a & m)!=0 || (b&m)!=0){
               if(carry==0)num= num | m;
-              else if(carry==1){
-                    num= num & ~(m);
-                  
-              }
+              else if(carry==1)num= num & ~(m);
             }
+          
+             // dealing when both of the  bits of this number are 0 
+          //then also we deal with 2 cases :- one is where we have an carry and second where there is no carry
             else if((a & m)==0 && (b&m)==0){
               if(carry==1){
                   num= num | m;
                   carry=0;
               }
             }
-          cout<<num<<endl;
+          // cout<<num<<endl;
         }
       return num;
     }
