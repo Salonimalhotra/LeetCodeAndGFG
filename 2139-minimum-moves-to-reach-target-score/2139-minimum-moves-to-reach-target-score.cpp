bb6bcf14c930e7963bfeb77446e39c0f4ee798fb
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-01 14:11:35 +0530

    Time: 2031 ms (5.10%), Space: 5.8 MB (73.94%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minMoves(int target, int maxDoubles) {
-        int minAns=0;
-        int start=1;
-        if(maxDoubles==0) return target-1;
-        else if(target==1) return 0;
+              if(maxDoubles==0) return target-1;
+
+               int minAns=0;
+               int start=1;
+      
         while(start!=target){
              if(maxDoubles!=0 && target%2==0 && target/2>=start){
                  target=target/2;
