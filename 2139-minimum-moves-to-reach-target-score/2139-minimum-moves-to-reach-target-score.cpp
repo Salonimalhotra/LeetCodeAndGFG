004efb1e50860ff7a02165551f51392c38cb3a64
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-01 14:10:24 +0530

    Time: 1575 ms (5.54%), Space: 5.9 MB (26.60%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int minAns=0;
+        int start=1;
+        if(maxDoubles==0) return target-1;
+        while(start!=target){
+             if(maxDoubles!=0 && target%2==0 && target/2>=start){
+                 target=target/2;
+                 maxDoubles--;
+                 minAns++;
+             }
+             else{
+                target=target-1;
+                minAns++;
+             }
+        }
+      return minAns;
+    }
+};
