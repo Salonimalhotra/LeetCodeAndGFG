f87085e29cb7c4defe8d7c51041d2281be114955
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-05 13:46:39 +0530

    Time: 269 ms (91.19%), Space: 11.1 MB (99.35%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,32 +1,52 @@
-class Solution {
-public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-       int **dp=new int *[nums1.size()+1];
-       for(int i=0;i<=nums1.size();i++){
-         dp[i]=new int [nums2.size()+1];
-       }
-      
-       for(int i=0;i<=nums1.size();i++){
-         for(int j=0;j<=nums2.size();j++){
-            if(i==0 || j==0){
-              dp[i][j]=0;
+class Solution
+{
+    public:
+        int findLength(vector<int> &nums1, vector<int> &nums2)
+        {
+            vector<int>prev(nums2.size()+1,0);
+            vector<int>curr(nums2.size()+1,0);
+//             int **dp = new int *[nums1.size() + 1];
+//             for (int i = 0; i <= nums1.size(); i++)
+//             {
+//                 dp[i] = new int[nums2.size() + 1];
+//             }
+
+//             for (int i = 0; i <= nums1.size(); i++)
+//             {
+//                 for (int j = 0; j <= nums2.size(); j++)
+//                 {
+//                     if (i == 0 || j == 0) dp[i][j] = 0;
+//                 }
+//             }
+
+            // int maxlength = 0;
+            // for (int i = 1; i <= nums1.size(); i++)
+            // {
+            //     for (int j = 1; j <= nums2.size(); j++)
+            //     {
+            //         if (nums1[i - 1] == nums2[j - 1])
+            //         {
+            //             dp[i][j] = 1 + dp[i - 1][j - 1];
+            //             maxlength = max(maxlength, dp[i][j]);
+            //         }
+            //         else dp[i][j] = 0;
+            //     }
+            // }
+           
+          int maxlength=0;
+          for(int i=1;i<=nums1.size();i++){
+            for(int j=1;j<=nums2.size();j++){
+               if(nums1[i-1]==nums2[j-1]){                
+                  curr[j]=1 + prev[j-1];
+                   maxlength = max(maxlength, curr[j]);                  
+               }
+              else{
+                curr[j]=0;
+              }
             }
-         }
-       }
-      
-      int maxlength=0;
-      for(int i=1;i<=nums1.size();i++){
-        for(int j=1;j<=nums2.size();j++){
-           if(nums1[i-1]==nums2[j-1]){
-               dp[i][j]=1 + dp[i-1][j-1];
-               maxlength=max(maxlength,dp[i][j]);
-            }
-          else{
-             dp[i][j]=0;
+            prev=curr;
+          
           }
+            return maxlength;
         }
-      }
-      
-      return maxlength;
-    }
 };
