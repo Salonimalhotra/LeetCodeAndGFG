2ab27016112f3bf6e9f89724abddcab4608fd781
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-11 12:10:09 +0530

    Time: 1533 ms (13.09%), Space: 571.9 MB (5.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,32 +1,49 @@
 class Solution
 {
     public:
-        int helper(vector<int> &nums, int curr, int prev, vector<vector< int>> &dp)
+       	//         int helper(vector<int> &nums, int curr, int prev, vector<vector < int>> &dp)
+       	//         {
+       	//             if (curr == nums.size())
+       	//             {
+       	//                 return 0;
+       	//             }
+       	//             if (dp[curr][prev + 1] != -1) return dp[curr][prev + 1];
+
+       	//             int option1 = 0;
+       	//             int option2 = 0;
+
+       	//            	//not take waala case
+       	//             option2 = helper(nums, curr + 1, prev, dp);
+
+       	//             if (prev == -1 || nums[curr] > nums[prev])
+       	//             {
+       	//                	// take waala case can only happen when there is no element in the sequence or the curr element is smaller then the element at previous index
+       	//                 option1 = 1 + helper(nums, curr + 1, curr,dp);
+       	//             }
+
+       	//             return dp[curr][prev + 1] = max(option1, option2);
+       	//         }
+        int tab(vector<int> &nums)
         {
-            if (curr == nums.size())
+            vector<vector < int>> dp(nums.size() + 1, vector<int> (nums.size() + 1, 0));
+            for (int curr = nums.size() - 1; curr >= 0; curr--)
             {
-                return 0;
+                for (int prev = curr-1 ; prev >= -1; prev--)
+                {
+                    int not_take=dp[curr+1][prev+1];
+                    int take=0;
+                    if(prev==-1 || nums[prev]<nums[curr]){
+                       take=1 + dp[curr+1][curr+1];
+                    }
+                  dp[curr][prev+1]=max(take,not_take);
+                }
             }
-            if (dp[curr][prev + 1] != -1) return dp[curr][prev + 1];
-
-            int option1 = 0;
-            int option2 = 0;
-
-           	//not take waala case
-            option2 = helper(nums, curr + 1, prev, dp);
-
-            if (prev == -1 || nums[curr] > nums[prev])
-            {
-               	// take waala case can only happen when there is no element in the sequence or the curr element is smaller then the element at previous index
-                option1 = 1 + helper(nums, curr + 1, curr,dp);
-            }
-
-            return dp[curr][prev + 1] = max(option1, option2);
+            return dp[0][0];
         }
     int lengthOfLIS(vector<int> &nums)
     {
 
         vector<vector < int>> dp(nums.size(), vector<int> (nums.size() + 1, -1));
-        return helper(nums, 0, -1, dp);
+        return tab(nums);
     }
 };
