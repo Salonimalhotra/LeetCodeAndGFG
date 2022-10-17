b82b92252ba7e641bf2f9585e2f2649e6acc284b
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-17 11:49:32 +0530

    Time: 26 ms (54.11%), Space: 13.6 MB (50.94%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,32 +1,51 @@
 class Solution
 {
     public:
-        int helper(vector<pair<int, int>> ans, int index,vector<int>&dp)
+        int tab(vector<pair<int, int>> ans)
         {
-            if (index == ans.size() - 1)
+            vector<int> dp(ans.size() + 1, 0);
+            dp[ans.size() - 1] = ans[ans.size() - 1].first *ans[ans.size() - 1].second;
+            for (int index = ans.size() - 2; index >= 0; index--)
             {
-                return ans[index].first *ans[index].second;
+                if (ans[index + 1].first == ans[index].first + 1)
+                {
+                    int option1 = ans[index].first *ans[index].second + dp[index + 2];
+                    int option2 = dp[index + 1];
+                    dp[index] = max(option1, option2);
+                }
+                else
+                {
+                    dp[index] = ans[index].first *ans[index].second + dp[index + 1];
+                }
             }
-          
-            if (index == ans.size())
-            {
-                return 0;
-            }            
-
-           if(dp[index]!=-1){
-             return dp[index];
-           }
-            if(ans[index+1].first==ans[index].first+1){
-               int option1 = ans[index].first *ans[index].second + helper(ans, index + 2,dp);
-               int option2 = helper(ans, index + 1,dp);
-                return dp[index]=max(option1, option2);    
-            }
-           
-           else{
-              return dp[index]=ans[index].first *ans[index].second + helper(ans, index + 1,dp);
-           }
-          
+            return dp[0];
         }
+   	//         int helper(vector<pair<int, int>> ans, int index,vector<int>&dp)
+   	//         {
+   	//             if (index == ans.size() - 1)
+   	//             {
+   	//                 return ans[index].first *ans[index].second;
+   	//             }
+
+   	//             if (index == ans.size())
+   	//             {
+   	//                 return 0;
+   	//             }            
+
+   	//            if(dp[index]!=-1){
+   	//              return dp[index];
+   	//            }
+   	//             if(ans[index+1].first==ans[index].first+1){
+   	//                int option1 = ans[index].first *ans[index].second + helper(ans, index + 2,dp);
+   	//                int option2 = helper(ans, index + 1,dp);
+   	//                 return dp[index]=max(option1, option2);    
+   	//             }
+
+   	//            else{
+   	//               return dp[index]=ans[index].first *ans[index].second + helper(ans, index + 1,dp);
+   	//            }
+
+   	//         }
     int deleteAndEarn(vector<int> &nums)
     {
         map<int, int> mp;
@@ -42,8 +61,8 @@ class Solution
             p1.second = i.second;
             ans.push_back(p1);
         }
-        vector<int>dp(ans.size(),-1);
-       return helper(ans,0,dp);
-      
+        // vector<int> dp(ans.size(), -1);
+        // return helper(ans, 0, dp);
+      return tab(ans);
     }
 };
