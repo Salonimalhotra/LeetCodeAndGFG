a922824bda32c38b92d8498f7e1a055f4a940ee0
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-10-18 15:11:00 +0530

    Time: 17 ms (32.21%), Space: 13.3 MB (35.85%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,70 @@
+class Solution
+{
+    public:
+        int orangesRotting(vector<vector < int>> &grid)
+        {
+            int fresh = 0;
+            queue<pair<int, int>> q;
+            int rows = grid.size();
+            int cols = grid[0].size();
+            vector<vector < bool>> visited(rows, vector<bool> (cols, false));
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (grid[i][j] == 2)
+                    {
+                        q.push(make_pair(i, j));
+                        visited[i][j] = true;
+                    }
+                }
+            }
+
+            while (q.size() != 0)
+            {
+                int s = q.size();
+                bool check=false;
+                while (s--)
+                {
+                    pair<int, int> top = q.front();
+                    q.pop();
+
+                    int dir[4][2] = {
+		{ 0,
+                            -1 },
+                        { 0,
+                            1 },
+                        { 1,
+                            0 },
+                        { -1,
+                            0 }
+                    };
+                    for (int i = 0; i < 4; i++)
+                    {
+                        int x = top.first + dir[i][0];
+                        int y = top.second + dir[i][1];
+                        if (x >= 0 && y >= 0 && x < rows && y < cols && visited[x][y] != true && grid[x][y] == 1)
+                        {
+                            check=true;
+                            q.push(make_pair(x, y));
+                            grid[x][y] = 2;
+                            visited[x][y] = true;
+                        }
+                    }
+                }
+                if(check==true)fresh++;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return fresh;
+        }
+};
