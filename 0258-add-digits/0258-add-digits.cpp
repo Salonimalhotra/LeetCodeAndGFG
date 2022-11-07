43e081783052645c635cfe2366f7866c96adce8b
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-11-07 15:42:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.34%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+        int addDigits(int num)
+        {
+            if (num >= 0 && num <= 9) return num;
+            else
+            {
+                int sum = 0;
+                while (num != 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                return addDigits(sum);
+            }
+        }
+};
