2a0c70974fb66735df2e367b0c39b6de0b181c21
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-11-13 14:26:08 +0530

    Time: 44 ms (50.00%), Space: 9.3 MB (50.00%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,57 +1,40 @@
 class Solution
 {
     public:
-        int subarrayLCM(vector<int> &nums, int m)
+        long long int gcd(int a, int b)
         {
-           	// int cnt = 0;
-           	// for (int i = 0; i < nums.size(); i++)
-           	// {
-           	//     for (int j = i; j < nums.size(); j++)
-           	//     {
-           	//         int minNum = INT_MAX;
-           	//         for (int k = i; k <= j; k++)
-           	//         {
-           	//             minNum = min(minNum, nums[k]);
-           	//         }
-           	//         int lcm = 1;
-           	//         for (int t = 1; t <= minNum; t++)
-           	//         {
-           	//             bool check = true;
-           	//             for (int k = i; k <= j; k++)
-           	//             {
-           	//                 if (nums[k] % t != 0)
-           	//                 {
-           	//                     check = false;
-           	//                     break;
-           	//                 }
-           	//             }
-           	//             if (check == true)
-           	//             {
-           	//                 lcm = t;
-           	//             }
-           	//         }
-           	//         if (lcm == m) cnt++;
-           	//     }
-           	// }
-           	// return cnt;
-            int cnt = 0;
-            for (int i = 0; i < nums.size(); i++)
+            if (b == 0)
             {
-                int lm = nums[i];
-                if(lm==m) cnt++;
-                for (int j = i+1; j < nums.size(); j++)
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+    long long int lcm(int a, int b)
+    {
+        return ((a / gcd(a, b)) *b);
+    }
+    int subarrayLCM(vector<int> &nums, int m)
+    {
+
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int lm = nums[i];
+            if (lm == m) cnt++;
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                lm = lcm(lm, nums[j]);
+                if (lm == m)
                 {
-                    lm = lcm(lm, nums[j]);
-                    if (lm == m)
-                    {
-                        cnt++;
-                    }
-                    else if (lm > m)
-                    {
-                        break;
-                    }
+                    cnt++;
+                }
+               	// we are breaking it as currently lcm is greater than m, therefore now it will only increase or remain constant, it cant decrease
+                else if (lm > m)
+                {
+                    break;
                 }
             }
-          return cnt;
         }
+        return cnt;
+    }
 };
