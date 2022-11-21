205987dbe0ea9dc371ce2b1ad341fc4e048fc6d9
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-11-21 20:02:28 +0530

    Time: 9 ms (51.38%), Space: 5.8 MB (71.71%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+        int arrangeCoins(int n)
+        {
+            if(n<0){
+              return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            int coins = n;
+            int rownum = 1;
+            while (coins != 0)
+            {
+                if (coins >= rownum)
+                {
+                    coins -= rownum;
+                    rownum++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return rownum-1;
+        }
+};
