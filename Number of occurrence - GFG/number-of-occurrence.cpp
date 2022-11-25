bfcadc4c26a52954780153cf280605734f457d19
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-11-25 12:00:02 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -1,73 +1,72 @@
-// { Driver Code Starts
-
+//{ Driver Code Starts
 
 #include<bits/stdc++.h>
 
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int firstOcc(int arr[],int n,int k){
-      int start=0;
-      int end=n-1;
-      int first=-1;
-      
-      while(start<=end){
-          int mid=start+(end-start)/2;
-          
-          if(arr[mid]==k){
-              first=mid;
-              end=mid-1;
-          }
-          else if(arr[mid]<k){
-              start=mid+1;
-          }
-          else {
-              end=mid-1;
-          }
-      }
-      return first;
-  }
-  
-  int lastOcc(int arr[],int n,int k){
-      int start=0;
-      int end=n-1;
-      int last=-1;
-      
-      while(start<=end){
-          int mid=start+(end-start)/2;
-          
-          if(arr[mid]==k){
-              last=mid;
-              start=mid+1;
-          }
-          else if(arr[mid]<k){
-              start=mid+1;
-          }
-          else {
-              end=mid-1;
-          }
-      }
-      return last;
-  }	
+    int firstOcc(int arr[],int n,int x){
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=(start+(end-start)/2);
+            if(arr[mid]<x){
+                start=mid+1;
+            }
+            else if(arr[mid]>x){
+                end=mid-1;
+            }
+            else{
+                if(mid==0 || arr[mid]!=arr[mid-1]){
+                    return mid;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    int lastOcc(int arr[],int n,int x){
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=(start+(end-start)/2);
+            if(arr[mid]<x){
+                start=mid+1;
+            }
+            else if(arr[mid]>x){
+                end=mid-1;
+            }
+            else{
+                if(mid==n-1 || arr[mid]!=arr[mid+1]){
+                    return mid;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
 	int count(int arr[], int n, int x) {
 	    // code here
 	    int first=firstOcc(arr,n,x);
 	    int last=lastOcc(arr,n,x);
-	    if(first==last && last==-1){
+	    if(first==-1){
 	        return 0;
 	    }
 	    return last-first+1;
-	    
 	}
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -85,4 +84,5 @@ int main() {
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
