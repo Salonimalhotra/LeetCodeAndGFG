e53ee8a3903301ef401facb8dbc2452de7582435
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-11-27 16:55:10 +0530

    Time: 108 ms (12.50%), Space: 30.6 MB (12.50%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,57 @@
+class Solution
+{
+    public:
+        int bestClosingTime(string customers)
+        {
+            int hours = customers.size();
+            vector<int> prefix;
+            int noCustomers = 0;
+            for (int i = 0; i <= customers.size(); i++)
+            {
+                prefix.push_back(noCustomers);
+                if (i < customers.size() && customers[i] == 'N')
+                {
+                    noCustomers++;
+                }
+            }
+
+            vector<int> suffix;
+            int yescustomers = 0;
+            suffix.push_back(yescustomers);
+            for (int i = customers.size() - 1; i >= 0; i--)
+            {
+                if (customers[i] == 'Y')
+                {
+                    yescustomers++;
+                }
+                suffix.push_back(yescustomers);
+            }
+            reverse(suffix.begin(), suffix.end());
+          
+            // cout << "prefix";
+            // for (int i = 0; i < prefix.size(); i++)
+            // {
+            //     cout << prefix[i] << " ";
+            // }
+            // cout << endl;
+            // cout << "suffix";
+            // for (int i = 0; i < suffix.size(); i++)
+            // {
+            //     cout << suffix[i] << " ";
+            // }
+            // cout << endl;
+          
+          
+            int best_ans = -1;
+            int score = INT_MAX;
+            for (int i = 0; i <= hours; i++)
+            {
+                if ((prefix[i] + suffix[i]) < score)
+                {
+                    best_ans = i;
+                    score = prefix[i] + suffix[i];
+                }
+            }
+            return best_ans;
+        }
+};
