ad751659d0824614c853837275bbc63cf38c5672
Saloni Malhotra <salonimalhotra2001@gmail.com>
2022-12-31 15:38:52 +0530

    Time: 157 ms (73.89%), Space: 81.2 MB (10.44%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long  sum=0;
+        long long sum=0;
+        vector<long long>prefixSum(chalk.size(),0);
         for(int i=0;i<chalk.size();i++){
           sum+=chalk[i];
+          prefixSum[i]=sum;
         }
       
         k=k%sum;
-        for(int i=0;i<chalk.size();i++){
-          if(chalk[i]<=k){
-            k-=chalk[i];
-          }
-          else{
-            return i;
-          }
-        }    
-        return -1;
+        // for(int i=0;i<chalk.size();i++){
+        //   if(chalk[i]<=k){
+        //     k-=chalk[i];
+        //   }
+        //   else{
+        //     return i;
+        //   }
+        // }    
+      int ans=upper_bound(prefixSum.begin(),prefixSum.end(),k)-prefixSum.begin();
+      return ans;
+       
     }
 };
