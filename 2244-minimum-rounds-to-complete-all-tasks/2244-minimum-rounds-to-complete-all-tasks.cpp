2ac5fc9d0971d4472e0cab8adbbae9c693f9bfdf
Saloni Malhotra <salonimalhotra2001@gmail.com>
2023-01-04 11:42:57 +0530

    Time: 237 ms (83.16%), Space: 103.7 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -19,26 +19,12 @@ class Solution
                 }
                 else
                 {
-                    while (x > 0)
-                    {
-                        if (x >= 3)
-                        {
-                            ans++;
-                            x -= 3;
-                        }
-                        else if (x <= 2)
-                        {
-                            ans++;
-                            if (x == 2)
-                            {
-                                x -= 2;
-                            }
-                            else
-                            {
-                                x -= 1;
-                            }
-                        }
-                    }
+                  if(x%3==0){
+                    ans+=x/3;
+                  }
+                  else{
+                    ans+=x/3 + 1;
+                  }
                 }
             }
             return ans;
