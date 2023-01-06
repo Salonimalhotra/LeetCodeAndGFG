c2f6e3da71c2c01451ffd69411a628b82bb57466
Saloni Malhotra <salonimalhotra2001@gmail.com>
2023-01-06 08:11:08 +0530

    Time: 255 ms (47.84%), Space: 76.3 MB (95.51%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,37 +1,24 @@
-class Solution {
-public:
-//     int tabulation(vector<int>& costs, int coins){
-//         vector<int>prev(coins+1,0);
-//         vector<int>curr(coins+1,0);        
-
-//         for(int i=1;i<=costs.size();i++){
-//             for(int j=1;j<=coins;j++){
-//                 if(costs[i-1]<=j){
-//                     curr[j]=max(1 + prev[j-costs[i-1]],curr[j]);
-//                 }
-//                 else{
-//                         curr[j]=prev[j];
-//                 }
-//             }
-//             prev=curr;
-//         }
-//         return prev[coins];
-//     }
-    int maxIceCream(vector<int>& costs, int coins) {
-        // return tabulation(costs,coins);   
-        sort(costs.begin(),costs.end());
-        int cnt=0;
-        int i=0;
-        while(coins>=0 && i<costs.size()){
-            if(costs[i]<=coins){
-                coins-=costs[i];
-                i++;
-                cnt++;
+class Solution
+{
+    public:
+        int maxIceCream(vector<int> &costs, int coins)
+        {
+            sort(costs.begin(), costs.end());
+            int ans = 0;
+            int i = 0;
+            while (i < costs.size())
+            {
+                if (costs[i] <= coins)
+                {
+                    coins -= costs[i];
+                    ans++;
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
             }
-            else{
-                break;
-            }
+            return ans;
         }
-        return cnt;
-    }
 };
